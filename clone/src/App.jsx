01c8afc9881b91b9ac54a4027adf6db71e48a892
Varun2024 <marvellous079@gmail.com>
2024-12-10 21:17:18 +0530

    bitofFrontend

--- clone/src/App.jsx
+++ clone/src/App.jsx
@@ -1,29 +1,57 @@
 
 import './App.css'
+import gptLogo from './assets/chatgpt.svg'
+import addBtn from './assets/add-30.png'
+import msgIcon from './assets/message.svg'
+import home from './assets/home.svg'
+import saved from './assets/bookmark.svg'
+import rocket from './assets/rocket.svg'
 
 function App() {
   
 
   return (
-    <>
-      <div className="sidebar">
+    <div className='App'>
+
+      <div className="sideBar">
+
         <div className="upperside">
+
           <div className="uppersidetop">
-            <img src="" alt="" />
+            <img src={gptLogo} alt="Logo" className='logo'/>
             <span className="brand">ChatGpt</span>
           </div>
+
           <button className="midBtn">
-            
+            <img src={addBtn} alt="new chat" className="addBtn" />
+            NewChat
           </button>
+          
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src={msgIcon} alt="query" className="" /> What is programming?
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="query" className="" /> How to use an API?
+            </button>
+
+          </div>
         </div>
         <div className="lowerside">
-           
+          <div className="listItems">
+            <img src={home} alt="" className="listItemsImage" />Home
+            <img src={saved} alt="" className="listItemsImage" />Saved
+            <img src={rocket} alt="" className="listItemsImage" />Upgrade to Pro
+          </div>
+
         </div>
+
       </div>
+
       <div className="main">
 
       </div>
-    </>
+    </div>
   )
 }
 
