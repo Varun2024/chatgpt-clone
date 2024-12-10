8b283c100b90a8e5d59fb3f41b819d1b759aa6b7
Varun2024 <marvellous079@gmail.com>
2024-12-10 11:37:20 +0530

    initial commit

---
+++ clone/src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="sidebar">
+        <div className="upperside">
+          <div className="uppersidetop">
+            <img src="" alt="" />
+            <span className="brand">ChatGpt</span>
+          </div>
+          <button className="midBtn">
+            
+          </button>
+        </div>
+        <div className="lowerside">
+           
+        </div>
+      </div>
+      <div className="main">
+
+      </div>
+    </>
+  )
+}
+
+export default App
